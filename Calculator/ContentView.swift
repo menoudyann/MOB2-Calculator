e8b0f8fa9104eb0ef40eea03663aa7142d4effe9
menoudyann <Yann.MENOUD@cpnv.ch>
2024-03-17 17:54:26 +0100

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Yann Menoud on 17.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
